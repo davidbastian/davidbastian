31dfe68d71f3f5b68cbab3f6e875d9ecf3acf933
davidbastian <d@davidbastian.cl>
2016-01-25 09:50:40 +1300

    added auto refresh browser

--- app/component.js
+++ app/component.js
@@ -3,7 +3,7 @@
 module.exports = function () {
   var element = document.createElement('h1');
 
-  element.innerHTML = 'Hello world';
+  element.innerHTML = 'Hello world22';
 
   return element;
 };
