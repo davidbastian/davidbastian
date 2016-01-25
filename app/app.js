cf4869de211641d30e56921c25e0590cf0dfe6ef
davidbastian <d@davidbastian.cl>
2016-01-25 23:48:44 +1300

    added sass and media

--- app/app.js
+++ app/app.js
@@ -1,30 +1,28 @@
+//load styles
+import './app.scss'
+
 //load angular
 import angular from 'angular'
 import ngroute from 'angular-route'
 import nganimate from 'angular-animate'
 import sanitize from 'angular-sanitize'
 
-
 //load gsap & pageAnimation
 import gsap from 'gsap'
 import {pageAnimation} from  './pageAnimation'
 
-
 //load routing
 import {pageRouting} from  './pageRouting'
 
-
 //load filters
 import {toSlug} from  './toSlug'
-
 
 //load controllers
 import {HomeController} from  './home/home.controller'
 import {AboutController} from  './about/about.controller'
 import {SingleController} from  './single/single.controller'
 
-
-//start angular
+//start app
 angular
 	.module("db", [ngroute, nganimate, sanitize])
 	.animation('.main-content',pageAnimation)
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,23 +1,36 @@
+var webpack = require('webpack')
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 
 var config = {
     entry: ['webpack/hot/dev-server', path.resolve(__dirname, '../app/app.js')],
-  output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: 'bundle.js'
-  },
+    output: {
+        path: path.resolve(__dirname, '../build'),
+        filename: 'bundle.js'
+    },
     module: {
         loaders: [{
-            test: /\.html$/, // Only .html files
-            loader: 'raw-loader'
-        },{
+                test: /\.scss$/,
+                loaders: [
+                    'style',
+                    'css',
+                    'autoprefixer?browsers=last 3 versions',
+                    'sass?outputStyle=expanded',
+                ]
+            }, {
+                test: /\.html$/, // Only .html files
+                loader: 'raw-loader'
+            }, {
 
-        	loader: "babel-loader",
-      		test: /\.jsx?$/,
-      		query: {
-        		presets: ['es2015', 'stage-0']
-      		}
-        }
+                loader: "babel-loader",
+                test: /\.jsx?$/,
+                query: {
+                    presets: ['es2015', 'stage-0']
+                }
+            }, {
+                test: /\.css$/,
+                loader: "style-loader!css-loader?importLoaders=1"
+            }
 
         ]
 
