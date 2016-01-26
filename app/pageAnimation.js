eee4cd0e0cf137ca48b879f6072928d1f50fb423
David Bastian <d@davidbastian.cl>
2016-01-26 17:51:39 +1300

    no more for today

--- app/pageAnimation.js
+++ app/pageAnimation.js
@@ -1,13 +1,32 @@
 function pageAnimation() {
         return {
-            /*enter: function(element, done) {
+            enter: function(element, done) {
+
+                console.log(element)
+
+
                 TweenMax.from(element, .5, {
                     opacity: 0,
                    // y: 50,
                     onComplete: done
                 })
-            },*/
+
+                /*TweenMax.to($('#single .white'), .5, {
+                    width: '100vw'
+                })*/
+
+            },
             leave: function(element, done) {
+                /*TweenMax.to($('#home'), .5, {
+                    x: '-20vw',
+                    opacity: 0
+                })*/
+
+                /*TweenMax.staggerTo(".media", 0.8, {opacity: 0, y: '30px'}, 0.2, done);
+
+                TweenMax.staggerTo(".media", 0.8, {opacity: 0, y: '30px'}, 0.2, done);*/
+
+
                 TweenMax.to(element, .5, {
                     opacity: 0,
                     //y: -50,
