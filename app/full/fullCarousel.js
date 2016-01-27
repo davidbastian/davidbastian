e5dc8810c7112976431d1de8d3307f7d7f4cb9f4
David Bastian <d@davidbastian.cl>
2016-01-27 16:58:23 +1300

    ok done loop

--- app/full/fullCarousel.js
+++ app/full/fullCarousel.js
@@ -8,9 +8,13 @@ function fullCarousel() {
         link: function(scope, element, attrs) {
 
             function functionToBeCalled(e) {
-                console.log(e)
+               // console.log(e)
                 if (!$('.wrap-a').last().hasClass('active')) {
                     $('.wrap-a.active').removeClass('active').next().addClass('active');
+                } else {
+                    $('.wrap-a.active').removeClass('active');
+                    $('.wrap-a').first().addClass('active');
+
                 }
             }
             element.on('click', functionToBeCalled);
