9b8806e2a7da22594f9c1f4170403c5de8995eef
davidbastian <d@davidbastian.cl>
2016-02-06 12:14:37 +1300

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -42,13 +42,7 @@ var config = {
             }, {
                 test: /\.css$/,
                 loader: "style-loader!css-loader?importLoaders=1"
-            }, {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&minetype=application/font-woff"
-            }, {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
-            },
+            }
 
         ]
 
